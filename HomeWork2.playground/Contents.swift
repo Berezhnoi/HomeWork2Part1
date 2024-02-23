25e9583e9d8270da4f39b3ed0f006b712b4c605f
IgorBerezhnoi <iberezhnoi@qualium-systems.com>
2024-02-23 19:52:38 +0100

    add comment

---
+++ HomeWork2.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+/* This is the beginning of the first multi-order comment.
+   /* This is another, rich-order comment's contributions. */
+   This is the first multi-row comment.
+*/
